88ead248a155e2a6c83a647e110327cab69eb258
joseph <langj6757@gmail.com>
2024-06-30 10:59:52 -0700

    Initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
