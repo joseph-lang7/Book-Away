c561ac0773d4daaabaacda40ae120c8ea936c286
joseph <langj6757@gmail.com>
2024-07-02 13:32:51 -0700

    Implement register functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,15 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-
+import userRoutes from "./routes/users";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api/users", userRoutes);
 
 app.listen(3001, () => {
   console.log("Server running on port 3001");
