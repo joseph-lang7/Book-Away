99dcbe8df6bb3819c6112aafd9ea950710bb1654
joseph <langj6757@gmail.com>
2024-07-02 19:37:38 -0700

    Implement login functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3001, () => {
   console.log("Server running on port 3001");
