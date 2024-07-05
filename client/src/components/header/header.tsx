d984c2f32fad8174e550443d14898232d0883033
joseph <langj6757@gmail.com>
2024-07-05 15:05:38 -0700

    Style links in header

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -12,13 +12,23 @@ const Header = () => {
         <span className="text-3xl text-white font-bold tracking-tight">
           <Link to="/">BookAway</Link>
         </span>
-        <span className="flex space-x-2">
+        <span>
           {isLoggedIn ? (
-            <>
-              <Link to="/my-bookings">My Bookings</Link>
-              <Link to="/my-bookings">My Hotels</Link>
+            <span className="flex gap-3">
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-blue-600 transition-colors"
+                to="/my-bookings"
+              >
+                My Bookings
+              </Link>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-blue-600 transition-colors"
+                to="/my-bookings"
+              >
+                My Hotels
+              </Link>
               <SignOutButton />
-            </>
+            </span>
           ) : (
             <SignInButton />
           )}
