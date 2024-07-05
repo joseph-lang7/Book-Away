9c1ded1255473e89aa41ec790b345bb1a5e9a1eb
joseph <langj6757@gmail.com>
2024-07-05 15:16:32 -0700

    Add register link to sign in page

--- client/src/pages/sign-in.tsx
+++ client/src/pages/sign-in.tsx
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/app-context";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 export type SignInFormData = {
   email: string;
   password: string;
@@ -63,16 +64,24 @@ const SignIn = () => {
           <p className="absolute text-red-500">{errors.password.message}</p>
         )}
       </label>
-      <span>
-        <button className="relative inline-flex items-center justify-center px-6 py-3 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group">
-          <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-blue-800 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
-          <span className="absolute inset-0 w-full h-full bg-white rounded-md "></span>
-          <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-blue-800 rounded-md opacity-0 group-hover:opacity-100 "></span>
-          <span className="relative text-blue-800 transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">
-            Sign In
-          </span>
-        </button>
-      </span>
+      <div className="flex justify-between items-start flex-col  gap-3">
+        <div className="flex gap-2 text-sm">
+          <span>Need an Account?</span>
+          <Link className="underline" to="/register">
+            Create an account here
+          </Link>
+        </div>
+        <span>
+          <button className="relative inline-flex items-center justify-center px-6 py-3 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group">
+            <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-blue-800 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+            <span className="absolute inset-0 w-full h-full bg-white rounded-md "></span>
+            <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-blue-800 rounded-md opacity-0 group-hover:opacity-100 "></span>
+            <span className="relative text-blue-800 transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">
+              Sign In
+            </span>
+          </button>
+        </span>
+      </div>
     </form>
   );
 };
