924d783cb5e384cca07a0d05696ebd93dab2039c
joseph <langj6757@gmail.com>
2024-07-09 21:17:41 -0700

    Make amenities section in add listing form responsive

--- client/src/forms/manage-listing-form/components/amenites-section.tsx
+++ client/src/forms/manage-listing-form/components/amenites-section.tsx
@@ -10,7 +10,7 @@ const AmenitiesSection = () => {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-3">Amenities</h2>
-      <div className="grid grid-cols-5 gap-3">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
         {listingAmenities.map((amenity) => (
           <label className="text-sm flex gap-1 text-gray-700" key={amenity}>
             <input
